096157ad0ed03ee2e58f172e7561571c666c4659
danielthegame <55468045+danielthegame@users.noreply.github.com>
2020-04-29 01:26:03 +0000

    Add files via upload
    Completed RobotColorChooser Assignment

--- src/_01_else_if/_1_robot_color_chooser/RobotColorChooser.java
+++ src/_01_else_if/_1_robot_color_chooser/RobotColorChooser.java
@@ -6,36 +6,63 @@ import javax.swing.JOptionPane;
 import org.jointheleague.graphical.robot.Robot;
 
 import java.awt.Color;
+import java.util.Random;
 
 public class RobotColorChooser {
 	public static void main(String[] args) {
 		
+		
+			
 		//1. Create a new Robot
 		Robot boby=new Robot();
+		
 	    //2. Make the robot draw a shape (this will take more than one line of code)
 		boby.penDown();
 		boby.move(100);
 		
 		//3. Set the pen width to 10
+		boby.setPenWidth(10);
 		
-		//4. Ask the user what color pen they would like the robot to draw with
-		String input=JOptionPane.showInputDialog("what is favorite color?");
-		//5. Use an if/else statement to set the pen color that the user requested
+		//7. Put a loop around your code so that you keep asking the user for more colors & drawing them
+		while(true)
+		{
+			
+			//4. Ask the user what color pen they would like the robot to draw with
+			String input=JOptionPane.showInputDialog("what is favorite color?");
+			
+			//5. Use an if/else statement to set the pen color that the user requested
+			if ( input.equalsIgnoreCase("Red")) 
+			{
+				boby.setPenColor(Color.red);  
+				boby.turn(-90);
+			} 
+			else if(input.equalsIgnoreCase("Green"))
+			{	
+				boby.setPenColor(Color.green);  
+				boby.turn(-105);
+			
+			}
+			else if(input.equalsIgnoreCase("Blue"))
+			{
+				boby.setPenColor(Color.blue);  
+				boby.turn(105);
+			}
+			else if (input.equals(""))
+			{
+				//6. If the user doesn’t enter anything, choose a random color
+				Random r = new Random();
+				boby.setPenColor(r.nextInt(255), r.nextInt(255), r.nextInt(255));
+				boby.turn(130);
+			}
+			else 
+			{
+				boby.setPenColor(Color.yellow);
+				boby.turn(90);
+			}	
+			
+			boby.move(200);
 		
-		
-		if ( input.equals("Red")) {
-			boby.setPenColor(Color.red);
-		} 
-				
-				
-				
-        //6. If the user doesn’t enter anything, choose a random color
-        //7. Put a loop around your code so that you keep asking the user for more colors & drawing them
-		
-
-		
-
-
-
+        
+		}
 	}
 }
