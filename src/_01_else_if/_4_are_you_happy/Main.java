b7210e5a26f97cabdfefdc56ea491c33f222365d
root <root@7fb067ce6964>
2020-06-03 01:29:29 +0000

    Performed additional review on if/else-if statements. Began a choose your own adventure assignment.

--- src/_01_else_if/_4_are_you_happy/Main.java
+++ src/_01_else_if/_4_are_you_happy/Main.java
@@ -10,7 +10,63 @@ public class Main {
 		//if - else if exercise
 		//String no = "";
 		
-		String answer = JOptionPane.showInputDialog(null,"Are you happy"); //IS THESE CORRECT?????
+		String answer = JOptionPane.showInputDialog(null,"Are you happy? or is your name bob?"); //IS THESE CORRECT?????
+		
+		
+		/*
+		 * no, yes, bob
+		 */
+		
+		/*
+		 * psudo code.. 
+		 * if - else statements are conditional dependent clauses.
+		 * As soon as the first if/else-if evaluates to true.. it will run its 
+		 * block, and then drop out of the entire if-else block..
+		 * 
+		 * int i = 1;
+		 * 
+		 * if(i == 0)  -> i is 1 NOT 0.. therefore false.
+		 * {
+		 * 	
+		 * 
+		 * } else if (i == 1 ) { it is 1 therefore true
+		 * 
+		 *  System.out.println("ITS TRUE!");
+		 *  
+		 *
+		 * } else if (i == 2) {
+		 * 
+		 * 
+		 * 
+		 * } else if (i >=3 ) {
+		 * 
+		 * 	
+		 * } else {
+		 * 
+		 * 		//Default catch all!
+		 * }
+		 * 
+		 * if - statements are not dependent clauses. Each one is evaluated 
+		 * separate from the previous if statement. if one evalutates to true,
+		 * the next statement will be evaluated independently as well.
+		 * 
+		 * if(i == 0) {
+		 * 		System.out.println("its 0");
+		 * }
+		 * 
+		 * if(i == 1) {
+		 * 		System.out.println("its 1");
+		 * }
+		 * 
+		 * if(i == 2) {
+		 * 		System.out.println("its 2");
+		 * }
+		 * 
+		 * if(i == 1 || i ==2) {
+		 * 
+		 * 		System.out.println("its 1 or 2");
+		 * }
+		 */
 		
 		if(answer.contentEquals("no"))
 		{
@@ -28,13 +84,14 @@ public class Main {
 		} else if(answer.contentEquals("yes")) {
 			
 			JOptionPane.showMessageDialog(null, "Keep on doing what you are doing");
-			
+		} else if(answer.contentEquals("bob")) {
+		
+			JOptionPane.showMessageDialog(null,"Nice you like bob too!!!");
+
 		} else {
 			
 			JOptionPane.showMessageDialog(null, "THEY ANSWERED ANYTHING OTHER THAN NO, YES, and BOB");
 		}
 		
-		//stuff for any answer happens here.
-		JOptionPane.showMessageDialog(null,"HAPPENS ALL THE TIME!");
 	}
 }
---
+++ src/_01_else_if/_5_own_adventure/Main.java
@@ -0,0 +1,48 @@
+package _01_else_if._5_own_adventure;
+
+import javax.swing.JOptionPane;
+
+public class Main {
+
+	public static void main(String[] args)
+	{
+		
+		/**
+		 * Daniel's Adventure!!!
+		 * 
+		 * Daniel's African Safari!
+		 * 
+		 * 
+		 * Top choice 3 levels
+		 * choice 1 -> 2 levels
+		 * choice 2 -> 2 levels
+		 * choice 3 -> 3 levels
+		 * 
+		 * Second tier
+		 * choice 1.A ->
+		 * choice 1.B ->
+		 * 
+		 * choice 2.A ->
+		 * choice 2.B ->
+		 * 
+		 * choice 3.A ->
+		 * choice 3.B ->
+		 * choice 3.C ->
+		 * 
+		 */
+		
+		String choice1 = JOptionPane.showInputDialog("You are standing patiently waiting outside the \n"
+				+ "African jeep safari tours headquarters in Tanzania. The air is fresh with the morning \n"
+				+ "dew lingering in the air. The sun has risen just above the horizon as you spot a jeep \n"
+				+ "rumbing towards you. You can't make out the color yet, but you're stoked to be going on \n"
+				+ "a safari. As it pulls up, the driver waves you over and excitedly asks you what adventure you \n"
+				+ "would like to take.  \n\nPrompt: \nEnter 1 for Lions and nasty saviars. \nEnter 2 for Gorillas and "
+				+ "Alligators. \nEnter 3 for Elephants and Giraffes.");
+		
+		
+		if(choice1.equals("1"))
+		{
+			//JOption
+		}
+	}
+}
