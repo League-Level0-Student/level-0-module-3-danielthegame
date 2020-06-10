a3a6343037dcc1c26ec22bfff39721943fd1b048
root <root@3d741c159063>
2020-06-10 01:26:12 +0000

    Added more to the adventure.. bit more to go.

--- src/_01_else_if/_5_own_adventure/Main.java
+++ src/_01_else_if/_5_own_adventure/Main.java
@@ -42,7 +42,67 @@ public class Main {
 		
 		if(choice1.equals("1"))
 		{
-			//JOption
+			String choice2 = JOptionPane.showInputDialog("The lion was so slick and you where just chilling then the lion \n"
+					+ "came and you ran away you where so scared the lion was so hungry and then the lion \n"
+					+ "Was so close to you and there was two paths one path was safe and the other \n"
+					+ "path was bad witch one will you pick \n\n"  
+					+ "Enter 1 if you want to go to the river full of poisonous water. \nEnter 2 if you want to go to the Volcano "
+					+ "full of lava.");
+			
+			if(choice2.contentEquals("1")) {
+				
+				JOptionPane.showMessageDialog(null,"YOU DIED!!!!!!!!!");	
+			
+			} else if(choice2.contentEquals("2")) {
+				
+				JOptionPane.showMessageDialog(null, "YOU DIED!!!!!!!");
+				
+			} else {
+				
+				JOptionPane.showMessageDialog(null, "The road less traveled is the path to safety\n"
+						+ "The road leads back to the Jeep safari headquarters where you are thankful \n"
+						+ "to be alive.");
+			}
+		} else if(choice1.contentEquals("2")) {
+			
+			String choice3 = JOptionPane.showInputDialog("The gorillas see that you are eating a bannana and they "
+					+ "start to chase you \nThe gorilla took your bannana \nwhat would you do.\n\n"
+					+ "Enter 1 if you would fight back.\n"
+					+ "Enter 2 if you would be chill.");
+			
+			if(choice3.equals("1")) {
+				
+				JOptionPane.showMessageDialog(null,"You went and tried to get your bannana back but then the\n"
+						+ "gorilla punched you and YOU DIED!!!!!!!!!!!!");
+			}
+			
+			if(choice3.contentEquals("2")) {
+				//JOption
+			}
 		}
 	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
