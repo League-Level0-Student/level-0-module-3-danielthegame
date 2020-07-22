6ff05bd5b39d9efa34b7f4fc6621f6173fa8e890
root <root@adb0b1f3f0c3>
2020-07-22 01:25:07 +0000

    Finished up 99 bottles

--- src/_02_loop_variables/_4_99_bottles/Bottles.java
+++ src/_02_loop_variables/_4_99_bottles/Bottles.java
@@ -6,23 +6,33 @@ public class Bottles {
 	{
 		
 		/*
-		 * Print the lyrics to 99 bottles of beer on the wall in a for loop.
-		 
-		 * I.E.
-		 * 99 bottles of beer on the wall, 99 bottles of beer.
-		 * Take one down and pass it around, 98 bottles of beer on the wall.
+		 * Print the lyrics to 99 bottles of juice boxes on the wall in a for loop.
+		 * 
 		 *
-		 * 98 bottles of beer on the wall, 98 bottles of beer.
-		 * Take one down and pass it around, 97 bottles of beer on the wall.
+		 * I.E.
+		 * 99 bottles of beer on the wall, 99 bottles of juice boxes.
+		 * Take one down and pass it around, 98 bottles of juice boxes on the wall.
+		 *
+		 * 98 bottles of beer on the wall, 98 bottles of juice boxes.
+		 * Take one down and pass it around, 97 bottles of juice boxes on the wall.
 		 *	
-		 * 97 bottles of beer on the wall, 97 bottles of beer.
-		 * Take one down and pass it around, 96 bottles of beer on the wall.
+		 * 97 bottles of beer on the wall, 97 bottles of juice boxes.
+		 * Take one down and pass it around, 96 bottles of juice boxes on the wall.
 		 */
-		for(int beersNotDrank = 99; beersNotDrank>0; beersNotDrank--) {
+		for(int juiceBoxesNotDrank = 99; juiceBoxesNotDrank > 0; juiceBoxesNotDrank--) {
+			
+			String myBox = "boxes";
+			
+			if(juiceBoxesNotDrank == 1) {
+				 
+				myBox = "box";
+				//System.out.println(myBox);
+			}
 		
-			System.out.println(beersNotDrank + " bottles of beer on the wall, " + beersNotDrank + 
-					" bottles of beer.\n"
-					+ "Take one down and pass it around, 98 bottles of beer on the wall.\n");
+			System.out.println(juiceBoxesNotDrank + " juice " + myBox + " on the wall, " + juiceBoxesNotDrank + 
+					" juice " + myBox + ".\n"
+					+ "Take one down and pass it around, " + (juiceBoxesNotDrank-1) + " juice boxes on the wall.\n");
+			
 		}
 		
 	}
